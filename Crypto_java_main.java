0d237793f851151579f330fce66a9028f4c4e27a
webguybit <xclusive.shakil@live.com>
2019-07-02 11:29:35 +0800

    Delete Crypto_java_main.java

--- Crypto_java_main.java
+++
@@ -1,5 +0,0 @@
-KeyGenerator kgen = KeyGenerator.getInstance("AES");
-SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
-secureRandom.setSeed(keyWord.getBytes());
-kgen.init(128, secureRandom);
-SecretKey secretKey = kgen.generateKey();
